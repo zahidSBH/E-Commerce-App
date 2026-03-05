915d652a59e9667dd631ab1f032f89479b1b8d62
Zahid Hasan <zahidhasanshellbeehaken@gmail.com>
2026-03-05 11:57:36 +0600

    fix the merge conflict in profile screen

--- Ecommerce/src/screens/profile/ProfileScreen.js
+++ Ecommerce/src/screens/profile/ProfileScreen.js
@@ -1,12 +1,7 @@
 import React from "react";
-<<<<<<< HEAD
 import { View, Text, StyleSheet } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import useUserProfile from "@/hooks/useUserProfile";
-=======
-import { Ionicons } from "@expo/vector-icons";
-import ScreenLabel from "@/components/ScreenLabel";
->>>>>>> main
 import theme from "@/constants/theme";
 
 const ProfileScreen = () => {
